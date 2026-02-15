4b46bb2b1716eb2a92c628a4fd4ba73363347b4d
sh4n66 <boteusefor6@gmail.com>
2026-02-15 12:21:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # List of channel/group IDs to monitor
 CHAT_IDS = [
-    -1001111111111,
+    -1003508437339,
     -1002222222222
 ]
 
@@ -11,7 +11,8 @@ ID_DUR = {
 }
 
 # Telegram API credentials (replace with your own in private .env or config file)
-API_ID = "YOUR_API_ID"
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = "32944672"
+API_HASH = "93a43ad9c58cb9fb522142298f2640a0"
+BOT_TOKEN = "8525441990:AAEbIy5xq__9ux7WYPnOmeYfpSAXpusJeD8"
+
 SESSION = "YOUR_SESSION_STRING"
