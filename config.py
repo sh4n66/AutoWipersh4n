bcfb2ec1363d16f4b9f042194d7d1abc5b47cf31
sh4n66 <boteusefor6@gmail.com>
2026-02-15 17:30:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # List of channel/group IDs to monitor
 CHAT_IDS = [
     -1003508437339,
-    -1002222222222
+    -1002944578653
 ]
 
 # Duration (in seconds) for each chat_id before messages are auto-deleted
 ID_DUR = {
     -1003508437339: 30,   # in seconds
-    -1002222222222: 180   # in seconds
+    -1002944578653: 180   # in seconds
 }
 
 # Telegram API credentials (replace with your own in private .env or config file)
@@ -16,5 +16,6 @@ API_HASH = "93a43ad9c58cb9fb522142298f2640a0"
 BOT_TOKEN = "8525441990:AAEbIy5xq__9ux7WYPnOmeYfpSAXpusJeD8"
 
 SESSION = "BQH2siAAY7GwgEm_idVRupD9X_btl-7tEDrLrTF1N0iEZDJyO_Vb7KwLUR68QnEzhAmg-L2N59sUcL4OOfASWLDZ9lyqRXohRQib5wi8LzcshvuaamVSfJU1qv1QK-s_znN22oN95sALwu1dgIg9sneon6ojFafBy2n6852v-fzb1O_fKx6jMgrlNikWez2rO0Q074tJSTEfcIb4Ab4Z6VHu2xxz1lfFVtwnhJzjBsVjAF6AxLDpK-DyKvGq4bIcUE3gIm-uzN-aZMeEGBNRx_NjalsgnGZzv-KfupXEINKFgn05GV-4T5X21TEOiScVJomdomG1RrFRZVleh-JBLWan1LqPSQAAAAF46JpxAA"
+
 
 
